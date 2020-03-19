7d8ce8d35905f331dfc404b51f5c8277291057e0
arobld00 <arobld00@gmail.com>
2020-03-20 00:46:29 +0100

    feat: Adding controller

--- src/main/java/com/idealista/infrastructure/api/AdsController.java
+++ src/main/java/com/idealista/infrastructure/api/AdsController.java
@@ -1,28 +1,60 @@
 package com.idealista.infrastructure.api;
 
-import java.util.List;
+import java.util.*;
 
+import com.idealista.infrastructure.api.viewmodel.PublicAd;
+import com.idealista.infrastructure.api.viewmodel.QualityAd;
+import com.idealista.infrastructure.services.listing.PublicAdService;
+import com.idealista.infrastructure.services.listing.QualityAdService;
+import com.idealista.infrastructure.services.scoring.ScoreAdService;
+import com.idealista.infrastructure.utils.errors.IncorrectScoringException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class AdsController {
 
-    //TODO añade url del endpoint
+    private ScoreAdService adScoring;
+
+    private PublicAdService publicAdListing;
+
+    private QualityAdService qualityAdListing;
+
+    public AdsController(ScoreAdService adScoring, PublicAdService publicAdListing, QualityAdService qualityAdListing) {
+        this.adScoring = adScoring;
+        this.publicAdListing = publicAdListing;
+        this.qualityAdListing = qualityAdListing;
+    }
+
+    //.antMatchers("/admin/*").hasRole("ADMIN");
+    @GetMapping("/admin/ads")
     public ResponseEntity<List<QualityAd>> qualityListing() {
-        //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
+        List<QualityAd> qualityList = this.qualityAdListing.find();
+        if (qualityList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else return new ResponseEntity<>(qualityList, HttpStatus.OK);
     }
 
-    //TODO añade url del endpoint
+    @GetMapping("/ads")
     public ResponseEntity<List<PublicAd>> publicListing() {
-        //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
+        List<PublicAd> publicList = this.publicAdListing.find();
+        if (publicList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else return new ResponseEntity<>(publicList, HttpStatus.OK);
     }
 
-    //TODO añade url del endpoint
+    @PostMapping("/ads/calculate")
     public ResponseEntity<Void> calculateScore() {
-        //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
+        try {
+            adScoring.execute();
+            return ResponseEntity.noContent().build();
+        } catch (IncorrectScoringException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Cannot be completed scoring", e);
+        }
     }
+
 }
